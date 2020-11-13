efffbfe663594882fd85036bc6aa60a77760d400
geomoetric <eric.g.moe@gmail.com>
2020-11-12 23:40:33 -0600

    feat(moelio2): Typekit & Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-86525313-1`,
       },
     },
     // `gatsby-plugin-feed`,
